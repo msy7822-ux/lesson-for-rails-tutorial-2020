374bbd506993b0370757d0b5fd72a927cc95d1b9
msy7822-ux <msy780022@gmail.com>
2020-09-04 01:31:19 +0000

    fixed code on routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root 'application#goodbyegit '
+  root 'application#goodbyegit'
   # 今回の例だと、次のようになる ==>> root 'コントローラ名#アクション名'
 end
